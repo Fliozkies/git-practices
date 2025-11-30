9c3f875eb81a553a150552834a817f75ad931393
Shuani <shuani@gmail.com>
2025-11-30 10:38:44 +0800

    Reapply "Merge pull request #1 from Fliozkies/feature-subtraction"
    This reverts commit bb03694189828431f8fe0b9aca20cd4ebc17c985.

--- Prac1.java
+++ Prac1.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
 public class Prac1 {
-    public static void Addition(Scanner scan) {
-        System.out.println("---Addition---");
+    private static void Calculate(int choice, Scanner scan) {
         System.out.print("Enter first number: ");
         int num1 = scan.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scan.nextInt();
 
-        System.out.println("The sum is: " + (num1 + num2));
+        switch (choice) {
+            case 1:
+                System.out.println("The sum is: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("The difference is: " + (num1 - num2));
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                break;
+        }
     }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         while (true) {
             System.out.println("----------Calculator Menu----------");
             System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("5. Exit");
             System.out.print("\nSelect: ");
             int choice = scan.nextInt();
             scan.nextLine();
@@ -24,7 +36,11 @@ public class Prac1 {
 
             switch (choice) {
                 case 1:
-                    Addition(scan); break;
+                    System.out.println("---Addition---");
+                    Calculate(choice, scan); break;
+                case 2:
+                    System.out.println("---Subtraction---");
+                    Calculate(choice, scan); break;
                 case 5:
                     System.out.println("Exiting the program.");
                     scan.close();
